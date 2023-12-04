7d73c1d412253f130a226f8c839f0b700bae52c0
rian256 <developer.rian@proton.me>
2023-12-04 12:46:28 +0700

    refactor

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
 require('options')
 require('plugins')
-require('lualine').setup{
-	options = { theme = 'ayu' }
-}
-require('nvim-tree').setup()
-local lspconfig = require('lspconfig') 
-lspconfig.lua_ls.setup{}
-lspconfig.gopls.setup{}
-
+require('config')
 
 vim.cmd("colorscheme ayu")
