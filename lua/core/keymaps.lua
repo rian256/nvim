8a79ed717f124fb21640ece97260b0ad8c609cf0
rian256 <developer.rian@proton.me>
2024-01-08 17:23:08 +0700

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,11 +5,9 @@ map("n", "<C-e>", "<cmd>NvimTreeToggle<CR>")
 
 map("n", "<C-r>", "<cmd>NvimTreeRefresh<CR>")
 
--- map("n", "<C-y>", "<cmd><CR>")
+-- Telescope
 local builtin = require("telescope.builtin")
 map("n", "<C-f>", builtin.find_files, {})
 map("n", "<C-r>", builtin.live_grep, {})
 map("n", "<C-b>", builtin.buffers, {})
 map("n", "<leader>fh", builtin.help_tags, {})
-
-map("n", "<C-s>", "<cmd>MarkdownPreview<CR>")
